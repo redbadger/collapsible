bbc4583966065338b23bfd313c006d203ebf88ac
Alex Savin <karismafilms@gmail.com>
2014-01-22 13:21:18 +0000

    Component initial version with test html

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-collapsible
+Component Collapsible
 ===========
 
-Component Collapsible
+Simple Bootstrap-like collapsible component for your DOM elements.
+
+##Usage
+
+* Tag the collapsible title with .collapse-toggle and data-collapse attribute equal to the collapsible selector
+* Require collapsible in your component or script
+* Call collapsible with two arguments - root selector and class name to be applied on the title when content is collapsed
+
+##Example
+
+JADE:
+
+    div.my-section
+      div.title.collapse-toggle(data-collapse="#collapse-container") Clickable title
+      div#filters-container
+        p Content to be collapsed
+
+LiveScript:
+
+    collapsible = require 'collapsible'
+    collapsible '.my-section', 'collapsed'
+
+Collapsible will parse all elements under `.my-section` root element and apply collapsing functionality to all `.collapse-toggle` elements.
---
+++ index.js
@@ -0,0 +1,45 @@
+var dom, event, hasClass, addClass, removeClass;
+dom = require('dom');
+event = require('event');
+hasClass = function(ele, cls){
+  return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+};
+addClass = function(ele, cls){
+  return ele.className += ' ' + cls;
+};
+removeClass = function(ele, cls){
+  var reg;
+  if (hasClass(ele, cls)) {
+    reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+    return ele.className = ele.className.replace(reg, ' ');
+  }
+};
+module.exports = function(target, klass){
+  var d, collapse, i$, len$, el, results$ = [];
+  console.log('hello');
+  klass == null && (klass = '');
+  d = dom(target);
+  collapse = d.find('.collapse-toggle');
+  console.log(collapse);
+  for (i$ = 0, len$ = collapse.length; i$ < len$; ++i$) {
+    el = collapse[i$];
+    console.log(el);
+    results$.push(event.bind(el, "click", fn$));
+  }
+  return results$;
+  function fn$(e){
+    var collapsible;
+    e.preventDefault;
+    collapsible = d.find(e.target.getAttribute("data-collapse"));
+    console.log(collapsible);
+    if (collapsible[0]) {
+      if (collapsible[0].style.display === 'block') {
+        collapsible[0].style.display = 'none';
+        return addClass(e.target, klass);
+      } else {
+        collapsible[0].style.display = 'block';
+        return removeClass(e.target, klass);
+      }
+    }
+  }
+};
